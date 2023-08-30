02f8efab4f8313fb954e23215027a45b4f066204
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-30 15:54:41 -0400

    tree structure rectified

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -3,13 +3,16 @@
 import s from "./Input.module.css"
 
 export default function Input() {
+
   return (
     <div className={s.formCont}>
       <form className={s.inputCont}>
         <input className={s.inputSpace} placeholder="Envía un mensage." type="text" />
-        <div className={s.submitButton}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"><path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="currentColor"></path></svg>  
-        </div>
+        <button className={s.submitButton}>
+          <div className={s.submitDiv}>
+            <svg xmlns="http://www.w3.org/2000/svg" color="var(--gray-600)" viewBox="0 0 16 16" fill="none"><path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="currentColor"></path></svg>  
+          </div>
+        </button>
       </form>
     </div>
   );
